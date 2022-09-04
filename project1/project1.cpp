b5733681eb1d13116b6041c16bad738f42f1c28a
Иван <ivanm1370@yandex.ru>
2022-09-04 15:20:27 +0300

    Добавьте файлы проекта.

---
+++ project1/project1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	float b, c, x = 0.0;
+	cout << "Привет! Эта программа решает уравнение вида bx + c = 0" << endl;
+	cout << "Задайте b: " << endl;
+	cin >> b;
+	cout << "Задайте c: " << endl;
+	cin >> c;
+
+	if (b == 0.0 && c == 0.0) {
+		cout << "X - любое число";
+	}
+	else if (b == 0.0) {
+		cout << "Решений нет" << endl;
+	}
+	else {
+		x = -c / b;
+		cout << "Ответ: " << x << endl;
+	}
+}
+
+
